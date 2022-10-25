de5f8baaccc8535822d98c5744beaffe83f3a824
Achintya151 <achintyakumartalukdar@gmail.com>
2022-10-26 00:15:45 +0600

    first commit

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,30 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layouts/Main";
+import Home from "../../Pages/Home/Home";
+import Login from "../../Pages/Login/Login/Login";
+import Register from "../../Pages/Login/Register/Register";
+
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/home',
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            }
+        ]
+    }
+]);
